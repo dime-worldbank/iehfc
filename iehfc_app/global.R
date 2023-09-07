3d670209ca602539cc46cd9633f083761b227f26
mfiorina <marcandreaw1@hotmail.com>
2023-09-07 10:12:38 -0400

    Added rmarkdown package load (main change)

--- iehfc_app/global.R
+++ iehfc_app/global.R
@@ -3,28 +3,27 @@
 # Purpose — Load packages and set parameters
 
   ## 1. Load Packages ----
-  
-  if(!require(pacman)) install.packages("pacman")
-  
-  pacman::p_load(shiny, dplyr, tidyr, purrr, ggplot2, janitor, data.table, DT, remotes, bsicons, shinydashboard, shinyjs)
-  
-  remotes::install_github(repo = "rstudio/bslib", quiet = TRUE)
 
-  library(bslib)
-  
+  if(!require(pacman)) install.packages("pacman")
+
+  pacman::p_load(shiny, dplyr, tidyr, purrr, ggplot2, janitor, data.table, DT, remotes, bsicons, shinydashboard, shinyjs, markdown)
+
+  remotes::install_github(repo = "rstudio/bslib", quiet = TRUE, upgrade = "never")
+
+  library(bslib, quietly = TRUE)
+
   ## 2. Load Custom Functions ----
-  
+
 # Reminder — Currently, this project works with an .Rproj file. Open the project directly from the .Rproj
 # file using the "File — Open Project..." option on RStudio or directly by clicking on the .Rproj file in
 # your file explorer. If you would rather not use the .Rproj file, you will need to set the working
 # directory using `setwd(...)`, with `...` being the file path that leads you to the `iehfc` folder.
-  
+
   source("R/iehfc_app.R")
-  
+
   ## 3. Run Application ----
-  
+
 # If you want to launch the iehfc application, you just need to type in `iehfc_app()` in your console after having
 # run the iehfc_app.R script as done in part 2. You can do this anytime.
-  
+
   iehfc_app()
-  
