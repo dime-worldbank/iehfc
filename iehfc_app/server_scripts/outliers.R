33bfbb5d749b229f0cf445f1e930811c38d94cbc
mfiorina <marcandreaw1@hotmail.com>
2023-09-19 16:41:07 -0400

    Small change to address duplicate IDs when identifying outliers

--- iehfc_app/server_scripts/outliers.R
+++ iehfc_app/server_scripts/outliers.R
@@ -20,6 +20,15 @@
       indiv_outlier_vars() %>%
           map(
               ~ hfc_dataset() %>%
+                  # Address any duplicates if there are some remaining, although they should be dealt with by this point
+                  group_by(!!sym(outlier_id_var())) %>%
+                  mutate(
+                      !!outlier_id_var() := case_when(
+                          n() > 1 ~ paste0(!!sym(outlier_id_var()), "_", row_number()),
+                          TRUE    ~ as.character(!!sym(outlier_id_var()))
+                      )
+                  ) %>%
+                  ungroup() %>%
                   mutate(
                       across(
                           matches(paste0("^", .x, "$")), ~ mean(.x, na.rm = TRUE), .names = "mean"
@@ -52,6 +61,15 @@
       group_outlier_vars() %>%
           map(
               ~ hfc_dataset() %>%
+                  # Address any duplicates if there are some remaining, although they should be dealt with by this point
+                  group_by(!!sym(outlier_id_var())) %>%
+                  mutate(
+                      !!outlier_id_var() := case_when(
+                          n() > 1 ~ paste0(!!sym(outlier_id_var()), "_", row_number()),
+                          TRUE    ~ as.character(!!sym(outlier_id_var()))
+                      )
+                  ) %>%
+                  ungroup() %>%
                   select(
                       any_of(outlier_id_var()), any_of(outlier_extra_vars()), matches(paste0("^", .x, "_{0,1}[0-9]+$"))
                   ) %>%
@@ -83,7 +101,8 @@
   bindEvent(input$run_hfcs)
   
   outlier_dataset <- reactive({
-      bind_rows(indiv_outlier_dataset(), group_outlier_dataset())
+      bind_rows(indiv_outlier_dataset(), group_outlier_dataset()) %>%
+          arrange(!!sym(outlier_id_var()), issue_var)
   }) %>%
   bindEvent(input$run_hfcs)
   
