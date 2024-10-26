db1a1e76965fbd106ab7daf6e9f429c67ba039f0
Mehrab Ali <mehrab.ali@arced.foundation>
2024-10-26 15:23:41 +0600

    Added Stata and R Code downloads
    1. Added Stata and R code download functionality
    2. Updated sample data (removed leading 0s from the prefixes of repeat variables)
    3.  Removed svg file type from gitignore file

--- iehfc_app/server_scripts/outliers.R
+++ iehfc_app/server_scripts/outliers.R
@@ -164,3 +164,163 @@
       outlier_dataset(), fillContainer = TRUE
   )
   
+  
+  
+  
+  ##### Download outlier codes ----
+  output$outlier_r_exp <- downloadHandler(
+      filename = function() {
+          "outlier_run.R"
+      },
+      content = function(file) {
+          code <- paste(
+              "# Remember to load your dataset \n",
+              "hfc_dataset <- \n",
+              "\n",
+              "# Outlier Variables \n",
+              "indiv_outlier_vars <- ", paste0("\"", input$indiv_outlier_vars_select_var, "\"", collapse = ", "), "\n",
+              "group_outlier_vars <- c(", paste0("\"", input$group_outlier_vars_select_var, "\"", collapse = ", "), ")\n",
+              "outlier_id_var <- c(", paste0("\"", input$outlier_id_select_var, "\"", collapse = ", "), ")\n",
+              "outlier_extra_vars <- c(", paste0("\"", input$outlier_extra_vars_select_var, "\"", collapse = ", "), ")\n",
+              "outlier_method_selected <- c(", paste0("\"", input$outlier_method, "\"", collapse = ", "), ")\n",
+              "outlier_multiplier_selected <- c(", paste0("\"", input$outlier_multiplier, "\"", collapse = ", "), ")\n",
+              "\n",
+              
+              "duplicate_dataset <- hfc_dataset %>% \n",
+              "    group_by(!!sym(duplicate_id_var)) %>% \n",
+              "    filter(n() > 1) %>% \n",
+              "    ungroup() %>% \n",
+              "    select(all_of(c(duplicate_id_var, duplicate_extra_vars))) \n",
+              " \n",
+              
+              "# Export to CSV  \n",
+              "write.csv(duplicate_dataset, \"outlier_table.csv\", row.names = FALSE) \n",
+              sep = ""
+          )
+          writeLines(code, file)
+      }
+  )
+  
+  
+  output$outlier_s_exp <- downloadHandler(
+      filename = function() {
+          "outlier_run.do"
+      },
+      content = function(file) {
+          code <- paste(
+              "    /*----------------------------------------------------\n",
+              "           This is code sample for duplicates check. \n",
+              "    -----------------------------------------------------*/ \n",
+              "\n",
+              "\n",
+              "    * Define the outlier variables\n",
+              "       local indiv_outlier_vars \"", paste0(input$indiv_outlier_vars_select_var, collapse = " "), "\"\n",
+              "       local group_outlier_vars \"", paste0(input$group_outlier_vars_select_var, collapse = " "), "\"\n",
+              "       local outlier_id_var \"", paste0(input$outlier_id_select_var, collapse = " "), "\"\n",
+              "       local outlier_extra_vars \"", paste0(input$outlier_extra_vars_select_var, collapse = " "), "\"\n",
+              "       local outlier_method_selected \"", paste0(input$outlier_method, collapse = " "), "\"\n",
+              "       local outlier_multiplier_selected \"", paste0(input$outlier_multiplier, collapse = " "), "\"\n",
+              "\n",
+              
+              "    * Create unique id, in case the id variable is not unique\n",
+              "    tostring `outlier_id_var', replace \n",
+              "    bysort `outlier_id_var': gen outlier_id = `outlier_id_var' + \"-\" + string(_n) if _N > 1 \n",
+              "    replace outlier_id = `outlier_id_var' if mi(outlier_id) \n",
+              "\n",
+              "    * Create list of group variables\n",
+              "    foreach var of loc group_outlier_vars { \n",
+              "        unab fullvarlist : `var'* \n",
+              "        loc groupvarlist = \"`groupvarlist' \" + \"`fullvarlist'\" \n",
+              "    } \n",
+              "\n",
+              "    * Create a tempfile to store the outlier values\n",
+              "    preserve \n",
+              "        clear \n",
+              "        tempfile outlier_file \n",
+              "        save `outlier_file', replace emptyok \n",
+              "    restore \n",
+              "\n",
+              "    * List of outliers for individual variables\n",
+              "    foreach var of loc indiv_outlier_vars { \n",
+              "        preserve \n",
+              "            keep outlier_id `outlier_id_var' `outlier_extra_vars' `var' \n",
+              "\n",
+              "            if \"`outlier_method_selected'\" == \"sd\" { \n",
+              "                qui sum `var' \n",
+              "\n",
+              "                g sd = r(sd) \n",
+              "                g low_limit = r(mean) - (`outlier_multiplier_selected' * r(sd)) \n",
+              "                g high_limit = r(mean) + (`outlier_multiplier_selected' * r(sd)) \n",
+              "                g mean = r(mean) \n",
+              "                g issue_var = \"`var'\" \n",
+              "                g value = `var' \n",
+              "            } \n",
+              "            else { \n",
+              "                egen iqr = iqr(`var') \n",
+              "                qui sum `var', d \n",
+              "\n",
+              "                g low_limit = r(p25) - (`outlier_multiplier_selected' * iqr) \n",
+              "                g high_limit = r(p75) + (`outlier_multiplier_selected' * iqr) \n",
+              "                g mean = r(mean) \n",
+              "                g issue_var = \"`var'\" \n",
+              "                g value = `var' \n",
+              "            } \n",
+              "\n",
+              "            keep if !inrange(value, low_limit, high_limit) & !mi(value) \n",
+              "            keep `outlier_id_var' `outlier_extra_vars' issue_var value mean `outlier_method_selected' low_limit high_limit \n",
+              "\n",
+              "            append using `outlier_file', nolabel nonotes \n",
+              "            save `outlier_file', replace \n",
+              "        restore \n",
+              "    } \n",
+              "\n",
+              "    * List of outliers for group variables\n",
+              "    foreach var of loc group_outlier_vars { \n",
+              "        preserve \n",
+              "            keep outlier_id `outlier_id_var' `outlier_extra_vars' `var'* \n",
+              "            reshape long `var'_, i(outlier_id) j(outliersl) \n",
+              "\n",
+              "            if \"`outlier_method_selected'\" == \"sd\" { \n",
+              "                qui sum `var' \n",
+              "\n",
+              "                g sd = r(sd) \n",
+              "                g low_limit = r(mean) - (`outlier_multiplier_selected' * r(sd)) \n",
+              "                g high_limit = r(mean) + (`outlier_multiplier_selected' * r(sd)) \n",
+              "                g mean = r(mean) \n",
+              "                g issue_var = \"`var'\" \n",
+              "                g value = `var' \n",
+              "            } \n",
+              "            else { \n",
+              "                egen iqr = iqr(`var') \n",
+              "                qui sum `var', d \n",
+              "\n",
+              "                g low_limit = r(p25) - (`outlier_multiplier_selected' * iqr) \n",
+              "                g high_limit = r(p75) + (`outlier_multiplier_selected' * iqr) \n",
+              "                g mean = r(mean) \n",
+              "                g issue_var = \"`var'\" \n",
+              "                g value = `var' \n",
+              "            } \n",
+              "\n",
+              "            keep if !inrange(value, low_limit, high_limit) & !mi(value) \n",
+              "            replace issue_var = \"`var'_\" + string(outliersl) \n",
+              "\n",
+              "            keep `outlier_id_var' `outlier_extra_vars' issue_var value mean `outlier_method_selected' low_limit high_limit \n",
+              "            append using `outlier_file', nolabel nonotes \n",
+              "            save `outlier_file', replace \n",
+              "        restore \n",
+              "    } \n",
+              "\n",
+              "    u `outlier_file', clear \n",
+              "    sort issue_var `outlier_id_var', stable \n",
+              "    list `outlier_id_var' `outlier_extra_vars' issue_var value mean `outlier_method_selected' low_limit high_limit, sepby(issue_var) \n",
+              "    order `outlier_id_var' `outlier_extra_vars' issue_var value  mean `outlier_method_selected' low_limit high_limit \n",
+              "\n",
+              "    * Export to CSV\n",
+              "    export delimited using \"outlier_table.csv\", replace \n",
+              sep = ""
+          )
+          writeLines(code, file)
+      }
+  )
+  
+  
--- iehfc_app/server_scripts/unit.R
+++ iehfc_app/server_scripts/unit.R
@@ -33,3 +33,70 @@
       unit_dataset(), fillContainer = TRUE, options = list(paging = FALSE)
   )
   
+  
+  ##### Download unit codes ----
+  output$unit_r_exp <- downloadHandler(
+      filename = function() {
+          "unit_run.R"
+      },
+      content = function(file) {
+          code <- paste(
+              "# Remember to load your dataset \n",
+              "hfc_dataset <- \n",
+              "\n",
+              "# Duplicate Variables \n",
+              "duplicate_id_var <- ", paste0("\"", input$duplicate_id_select_var, "\"", collapse = ", "), "\n",
+              "duplicate_extra_vars <- c(", paste0("\"", input$duplicate_extra_vars_select_var, "\"", collapse = ", "), ")\n",
+              "\n",
+              
+              "duplicate_dataset <- hfc_dataset %>% \n",
+              "    group_by(!!sym(duplicate_id_var)) %>% \n",
+              "    filter(n() > 1) %>% \n",
+              "    ungroup() %>% \n",
+              "    select(all_of(c(duplicate_id_var, duplicate_extra_vars))) \n",
+              " \n",
+              
+              "# Export to CSV  \n",
+              "write.csv(unit_dataset, \"unit_table.csv\", row.names = FALSE) \n",
+              sep = ""
+          )
+          writeLines(code, file)
+      }
+  )
+  
+  
+  output$unit_s_exp <- downloadHandler(
+      filename = function() {
+          "unit_run.do"
+      },
+      content = function(file) {
+          code <- paste(
+              "    /*----------------------------------------------------\n",
+              "           This is code sample for duplicates check. \n",
+              "    -----------------------------------------------------*/ \n",
+              "\n",
+              "\n",
+              "    * Define the duplicate variables\n",
+              "       local unit_var ", paste0(input$unit_var_select_var, collapse = " "), "\n",
+              "       local unit_extra_vars ", paste0(input$unit_extra_vars_select_var, collapse = " "), "\n",
+              "\n",
+              "    * Sort and identify duplicates\n",
+              "       bys `unit_var': gen _unit_order = _n \n",
+              "       loc unit_type: type `unit_var' \n",
+              "       if regex(\"`unit_type'\", \"str\"){ \n",
+              "            ren  `unit_var' `unit_var'__str \n",
+              "       }\n",
+              "       else tostring `unit_var', gen(`unit_var'__str)\n",
+              "\n",
+              "       replace `unit_var'__str = `unit_var'__str + \"_\" + string(_unit_order) \n",
+              "\n",
+              "    * Export to CSV\n",
+              "       keep `unit_var'__str `unit_extra_vars' \n",
+              "       ren `unit_var'__str `unit_var' \n",
+              "       keep `unit_var' `unit_extra_vars' \n",
+              "       export delimited using \"unit_table.csv\", replace\n",
+              sep = ""
+          )
+          writeLines(code, file)
+      }
+  )
