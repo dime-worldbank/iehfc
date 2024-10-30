e3c7aa542d9693f19df241bffee67ea6cc0f1ebc
Marina Visintini <140360330+m-visintini@users.noreply.github.com>
2024-10-30 13:32:03 -0400

    Revert "Electron test"
    This reverts commit 680f98be1f792740b48058d1fd39591abd7c8277.

---
+++ R/iehfc_app.R
@@ -0,0 +1,27 @@
+
+
+  iehfc_app <- function() {
+      
+      shiny::addResourcePath(prefix = "res", directoryPath = "iehfc_app/www")
+      
+      extract_source <- function(filename) {
+          source(filename, local = parent.frame(), chdir = TRUE)$value
+      }
+      
+      req_packages <- c(
+          "pacman", "shiny", "dplyr", "tidyr", "purrr", "ggplot2", "janitor", "data.table", "DT",
+          "remotes", "bsicons", "shinydashboard", "shinyjs", "markdown",
+          "htmlwidgets", "plotly"
+      )
+      
+      if(sum(req_packages %in% (.packages())) != length(req_packages)) { # Means we need to run global.R to get packages, otherwise we're fine
+          extract_source("iehfc_app/global.R")
+      }
+      
+      shinyApp(
+          ui = extract_source("iehfc_app/iehfc_ui.R"),
+          server = extract_source("iehfc_app/iehfc_server.R")
+      )
+      
+  }
+  
