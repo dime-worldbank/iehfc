fd9bda21a5e7e206b8e699bfa1f451a248663933
Marina Visintini <140360330+m-visintini@users.noreply.github.com>
2024-11-06 17:00:27 -0500

    posit error fixes 8

--- iehfc_app/ui.R
+++ iehfc_app/ui.R
@@ -4,83 +4,112 @@
 
 # libraries
 
-  library(shiny)
-  library(bslib)
-  library(DT)
-  library(shinyjs)
+library(shiny)
+library(bslib)
+library(DT)
+library(shinyjs)
 
 # read source scripts
 
-  source("ui_scripts/introduction_tab.R")
+source("ui_scripts/introduction_tab.R")
 
-  fluidPage(
-      useShinyjs(),
-      tags$head(
-          tags$script(HTML('
+fluidPage(
+    useShinyjs(),
+    tags$head(
+        tags$script(HTML('
             $(document).ready(function() {
-                $(".navbar .container-fluid .navbar-nav .dropdown .dropdown-menu").append(\'<li><a href="https://github.com/dime-worldbank/iehfc/blob/main/README.md" >Guides</a></li>\');
-                $(".navbar .container-fluid .navbar-nav .dropdown .dropdown-menu").append(\'<li><a href="https://www.github.com/dime-worldbank/iehfc">Github</a></li>\');
+                $(".navbar .container-fluid .navbar-nav .dropdown .dropdown-menu")
+                    .append(\'<li><a href="https://github.com/dime-worldbank/iehfc/blob/main/README.md" target="_blank">Guides</a></li>\');
+                $(".navbar .container-fluid .navbar-nav .dropdown .dropdown-menu")
+                    .append(\'<li><a href="https://www.github.com/dime-worldbank/iehfc" target="_blank">Github</a></li>\');
             });
-          '))),
-      navbarPage(
-          "IEHFC",
-          id = "tabs",
-          # Initialize shinyjs
-          tabPanel(
-              "Introduction",
-              introduction_tab 
-          ),
-          tabPanel(
-              "Upload Data",
-              value = "upload_tab",  # Give an ID for reference
-              uiOutput("upload_tab")
-          ),
-          tabPanel(
-              "Check Selection and Setup",
-              value = "setup_tab",  # Give an ID for reference
-              uiOutput("setup_tab")
-          ),
-          tabPanel(
-              "Outputs",
-              value = "output_tab",  # Give an ID for reference
-              uiOutput("output_tab")
-              
-          ),
-          nav_menu("More"
-          ),
-          
-          theme = bs_theme(
-              base_font    = font_google("Atkinson Hyperlegible"),
-              heading_font = font_google("Atkinson Hyperlegible"),
-              code_font    = font_google("Fira Code"), 
-              bootswatch = "pulse"
-          ) %>%
-              bs_add_rules(
-                  "
-                  h3 {
-                      font-size: 24px;
-                      font-weight: bold;
-                  }
+        '))
+    ),
+    navbarPage(
+        title = div(style = "white-space: nowrap;", "IEHFC"),
+        id = "tabs",
+        tabPanel("Introduction", introduction_tab),
+        tabPanel("Upload Data", value = "upload_tab", uiOutput("upload_tab")),
+        tabPanel("Check Selection and Setup", value = "setup_tab", uiOutput("setup_tab")),
+        tabPanel("Outputs", value = "output_tab", uiOutput("output_tab")),
+        nav_menu("More"),
+        theme = bs_theme(
+            base_font    = font_google("Atkinson Hyperlegible"),
+            heading_font = font_google("Atkinson Hyperlegible"),
+            code_font    = font_google("Fira Code"), 
+            bootswatch = "pulse"
+        ) %>%
+            bs_add_rules(
+                "
+                .navbar-header {
+                    max-width: 46px;
+                    display: flex;
+                    flex-direction: row;
+                    align-items: left !important;
+                    margin-right: 2rem;
+                }
 
-                  body {
-                      font-size: 16px;
-                  }
-                  
-                  .nav-link {
-                      font-size: 18px;
-                  }
-                  
-                  .shiny-notification {
-                       position:fixed;
-                       right: calc(65%);
-                  }
-                  
-                   .navbar, .navbar-light, .bg-light {
-        background-color: #9e83cf !important;  /* Purple background for main navbar */
-    }
-                  "
-              ) # Source: https://stackoverflow.com/questions/44112000/move-r-shiny-shownotification-to-center-of-screen
-      )
-      
-  )
-  
+                .navbar-nav {
+                    display: flex;
+                    flex-wrap: nowrap !important;
+                    flex-direction: row;
+                    align-items: left !important;
+                }
+
+                .navbar-brand {
+                    white-space: nowrap;
+                    font-size: 1rem;
+                    display: flex;
+                    max-width: 50px;
+                    align-items: left !important;
+                }
+
+                .navbar-nav .nav-link {
+                    font-size: calc(min(1vw + 8px, 16px));
+                    padding: 0.5rem 0.75rem;
+                    white-space: nowrap;
+                    max-width: 250px;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                }
+
+                .navbar, .navbar-light, .bg-light {
+                    background-color: #9e83cf !important;
+                }
+
+                @media (max-width: 780px) {
+                    .navbar-header {
+                        display: align-items;
+                        flex-direction: column !important;
+                        align-items: left !important;
+                    }
+
+                    .navbar-brand {
+                        align-self: left;
+                        margin-bottom: 1rem;
+                        align-items: left !important;
+                        margin-left: 2.5rem;
+                    }
+
+                    .navbar-nav {
+                        display: flex;
+                        flex-direction: column;
+                        align-items: left;
+                        min-width: 100%;
+                    }
+
+                    .navbar-nav .nav-item {
+                        text-align: left;
+                        align-items: left !important;
+                        width: 100%;
+                    }
+
+                    .navbar-nav .nav-link {
+                        align-items: left !important;
+                        width: 100%;
+                    }
+                }
+            "
+            )
+    )
+)
--- iehfc_app/ui_scripts/introduction_tab.R
+++ iehfc_app/ui_scripts/introduction_tab.R
@@ -2,61 +2,73 @@
 ############### INTRODUCTION TAB ###############
 ###############################################
 
-library("shinydashboard")
+library(shinydashboard)
 
-  introduction_tab <- shinydashboard::tabItem(
-      tabName = "introduction",
-      div(style = "text-align:left", tags$h2("Welcome to IEHFC!")),
-      fluidRow(
-          column(width = 6,
-              box(
-                  width = "100%", status = "success",
-                  shiny::img(src = "res/BULBS_purple.png", style = "width:220px;"),
-                  shiny::img(src = "res/WB_logo.png", style = "max-width:100%; margin-bottom:20px;"),
-              ),
-              div(style = "text-align:left", tags$h3("The Need for IEHFC")),
-              box(
-                  width = "100%", status = "success",
-                  p("IEHFC arose from the pressing need to standardize and simplify high-frequency data checks in international development research. It aims to bridge the gap between the established necessity of these checks and the complexity involved in implementing them."),
-                  p(
-                      "Developed by the team at",
-                      tags$a("DIME Analytics", href = "https://www.worldbank.org/en/research/dime/data-and-analytics"),
-                      ", IEHFC aims to be a one-stop solution for researchers, providing easy-to-create, customizable, and shareable high-frequency check outputs.",
-                      "For more information about high-frequency checks you can consult", tags$a("our wiki resource.", href = "https://dimewiki.worldbank.org/High_Frequency_Checks")
-                  ),
-                  p(
-                      "If you encounter any issues or have suggestions, please report them ", 
-                      tags$a("on our GitHub issues section.", href = "https://github.com/dime-worldbank/iehfc/issues")
-                  )
-              )
-          ),
-          column(width = 6,
-              div(style = "text-align:left", tags$h3("How IEHFC Works")),
-              box(
-                  width = "100%", status = "success",
-                  p("IEHFC is designed to be an intuitive platform that streamlines the data checking process. It features a sequential tab design that guides users through data uploading, check selection, parameter setup, and viewing the results.")
-              ),
-              div(style = "text-align:left", tags$h3("Features")),
-              box(
-                  width = "100%", status = "success", style = "margin-bottom: 0px;",   # Reduce bottom margin here
-                  p("IEHFC provides features to perform a comprehensive list of checks on your dataset, ranging from basic data integrity tests to advanced statistical checks."),
-                  #       shiny::img(src = "res/features.png", style = "max-width:500px;"), # here if we want a visual after feature
-              )
-          )
-      ),
-      fluidRow(
-          column(width = 6),
-          column(width = 6,
-              div(style = "text-align:left; margin-top: -190px;", tags$h3("Getting Started with IEHFC")),  # Reduce top margin here
-              box(
-                  width = "100%", status = "success",
-                  "Using IEHFC is as simple as:",
-                  tags$ul(
-                      tags$li(a(href="#", "Uploading your dataset", onclick = "Shiny.onInputChange('gotoTab', 'Upload Data')")),
-                      tags$li(a(href="#", "Selecting the desired checks", onclick = "Shiny.onInputChange('gotoTab', 'Check Selection and Setup')")),
-                      tags$li(a(href="#", "Reviewing and exporting your results", onclick = "Shiny.onInputChange('gotoTab', 'Outputs')"))
-                  )
-              )
-          )
-      )
-  )
+introduction_tab <- shinydashboard::tabItem(
+    tabName = "introduction",
+    
+    fluidRow(
+        column(
+            width = 6,
+            div(
+                style = "display: flex; align-items: center; justify-content: flex-start;",
+                div(style = "text-align:left;", tags$h1("Welcome to IEHFC!")
+            )
+        )
+        ),
+        column(
+            width = 6,
+            style = "display: flex; justify-content: flex-end; align-items: flex-end;",
+            shiny::img(src = "BULBS_purple.png", style = "width:180px; margin-right: 20px;"),
+            
+            )
+    ),
+    
+    div(style = "margin: 10px 0;", hr()),
+    
+    
+    fluidRow(
+        column(
+            width = 6,
+            div(style = "text-align:left", tags$h3("The Need for IEHFC")),
+            box(
+                width = 12, status = "success",
+                p("IEHFC arose from the pressing need to standardize and simplify high-frequency data checks in international development research. It aims to bridge the gap between the established necessity of these checks and the complexity involved in implementing them."),
+                p(
+                    "Developed by the team at",
+                    tags$a("DIME Analytics", href = "https://www.worldbank.org/en/research/dime/data-and-analytics"),
+                    ", IEHFC aims to be a one-stop solution for researchers, providing easy-to-create, customizable, and shareable high-frequency check outputs.",
+                    "For more information about high-frequency checks you can consult", tags$a("our wiki resource.", href = "https://dimewiki.worldbank.org/High_Frequency_Checks")
+                ),
+                p(
+                    "If you encounter any issues or have suggestions, please report them ", 
+                    tags$a("on our GitHub issues section.", href = "https://github.com/dime-worldbank/iehfc/issues")
+                )
+            ),
+            shiny::img(src = "WB_logo.png", style = "width: 200px; margin-top: 20px;")
+        ),
+        column(
+            width = 6,
+            div(style = "text-align:left", tags$h3("How IEHFC Works")),
+            box(
+                width = 12, status = "success",
+                p("IEHFC is designed to be an intuitive platform that streamlines the data checking process. It features a sequential tab design that guides users through data uploading, check selection, parameter setup, and viewing the results.")
+            ),
+            div(style = "text-align:left; margin-top: 20px;", tags$h3("Features")),
+            box(
+                width = 12, status = "success",
+                p("IEHFC provides features to perform a comprehensive list of checks on your dataset, ranging from basic data integrity tests to advanced statistical checks.")
+            ),
+            div(style = "text-align:left; margin-top: 20px;", tags$h3("Getting Started with IEHFC")),
+            box(
+                width = 12, status = "success",
+                "Using IEHFC is as simple as:",
+                tags$ul(
+                    tags$li(a(href = "#", "Uploading your dataset", onclick = "Shiny.onInputChange('gotoTab', 'Upload Data')")),
+                    tags$li(a(href = "#", "Selecting the desired checks", onclick = "Shiny.onInputChange('gotoTab', 'Check Selection and Setup')")),
+                    tags$li(a(href = "#", "Reviewing and exporting your results", onclick = "Shiny.onInputChange('gotoTab', 'Outputs')"))
+                )
+            )
+        )
+    )
+)
