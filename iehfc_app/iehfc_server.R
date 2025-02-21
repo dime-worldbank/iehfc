3497a973791ac512b02e0ec179fe904c74b24ead
Marina Visintini <140360330+m-visintini@users.noreply.github.com>
2025-02-21 12:33:08 -0500

    Bring updates from internal-distribution-main

--- iehfc_app/iehfc_server.R
+++ iehfc_app/iehfc_server.R
@@ -37,24 +37,72 @@
       
       hfc_dataset <- reactiveVal()
       
+      # observeEvent(input$hfc_file, {
+      #     ds <- fread(hfc_file()$datapath, na.strings = c("", "NA_character_", "NA"))
+      #     too_many_cols <- ncol(ds) > 10000
+      #     if(too_many_cols) {
+      #         showNotification(
+      #             "Your dataset has more than 10,000 variables. Unfortunately, the platform currently cannot handle such a large dataset. Please create a subset of your dataset and try again.",
+      #             duration = NULL,
+      #             type     = "error"
+      #         )
+      #     }
+      #     req(!too_many_cols)
+      #     hfc_dataset(ds)
+      # })
+      # 
+      # observeEvent(input$use_test_data, {
+      #     ds <- fread("test_data/LWH_FUP2_raw_data.csv", na.strings = "")
+      #     hfc_dataset(ds)
+      # })
+      
       observeEvent(input$hfc_file, {
-          ds <- fread(hfc_file()$datapath, na.strings = c("", "NA_character_", "NA"))
-          too_many_cols <- ncol(ds) > 10000
-          if(too_many_cols) {
-              showNotification(
-                  "Your dataset has more than 10,000 variables. Unfortunately, the platform currently cannot handle such a large dataset. Please create a subset of your dataset and try again.",
-                  duration = NULL,
-                  type     = "error"
-              )
+          req(input$hfc_file)
+          
+          file_path <- input$hfc_file$datapath
+          ds <- tryCatch(
+              data.table::fread(file_path),
+              error = function(e) {
+                  showNotification(paste("Error reading the uploaded file:", e$message), type = "error")
+                  return(NULL)
+              }
+          )
+          
+          if (!is.null(ds)) {
+              duplicated_cols <- names(ds)[duplicated(names(ds))]
+              
+              if (length(duplicated_cols) > 0) {
+                  showNotification(
+                      paste("The uploaded dataset has duplicate column names:", paste(duplicated_cols, collapse = ", "), ". Please ensure columns have unique names"),
+                      duration = NULL,
+                      type = "error"
+                  )
+              } else if (ncol(ds) > 10000) {
+                  showNotification(
+                      "Your dataset has more than 10,000 variables. Unfortunately, the platform currently cannot handle such a large dataset. Please create a subset of your dataset and try again.",
+                      duration = NULL,
+                      type = "error"
+                  )
+              }  else {
+                  hfc_dataset(ds) 
+              }
           }
-          req(!too_many_cols)
-          hfc_dataset(ds)
       })
       
       observeEvent(input$use_test_data, {
-          ds <- fread("test_data/LWH_FUP2_raw_data.csv", na.strings = "")
+          test_file_path <- file.path(getwd(), "test_data", "LWH_FUP2_raw_data.csv")
+          
+          ds <- tryCatch(
+              data.table::fread(test_file_path),
+              error = function(e) {
+                  showNotification("Error loading test data. Please check the file location.", type = "error")
+                  return(NULL)
+              }
+          )
+          
           hfc_dataset(ds)
       })
+      
       
       output$hfc_ds_display <- renderDT(
           hfc_dataset(), fillContainer = TRUE
