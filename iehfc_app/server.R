7c1519259f118030bd6facfb7b37e375b022a47f
Yukiko Suzuki <yukinko.iwasaki@gmail.com>
2025-10-14 17:04:45 -0400

    Update server.R

--- iehfc_app/server.R
+++ iehfc_app/server.R
@@ -187,44 +187,44 @@ pacman::p_load(
       })
 
       output$dataset_buttons <- renderUI({
-                if (is.null(hfc_dataset())) {
-                    tagList(
-                        card(
-                            fileInput(
-                                "hfc_file",
-                                label = span("Upload HFC Data", bsicons::bs_icon("question-circle-fill")) %>%
-                                    tooltip(
-                                        "This is where you upload the dataset on which you want to run data quality checks. In the next version of this application, there will be error checks at this point to make sure that the dataset is in the right format/encoding",
-                                        placement = "auto"
-                                    ),
-                                accept = ".csv",
-                                placeholder = "mydata.csv"
-                            ),
-                            span(
-                                "Currently, this application only accepts .csv files. Please make sure your file is in the .csv format before uploading.",
-                                style = "color: #593196; font-size: 12px;"
-                            )
-                        ),
-                        card(
-                            uiOutput("use_test_data_button")
-                        ),
-                        card(
-                            actionButton("show_databricks_modal", "Connect to Database", icon = icon("database"), class = "btn btn-outline-success btn-lg", width = "100%")
-                        )
-                    )
-                } else {
+            if (is.null(hfc_dataset())) {
+                tagList(
                     card(
-                        actionButton(
-                            "clear_dataset",
-                            "Restart with New Data",
-                            icon = icon("repeat"),
-                            class = "btn btn-outline-primary btn-lg",
-                            width = "100%"
+                        fileInput(
+                            "hfc_file",
+                            label = span("Upload HFC Data", bsicons::bs_icon("question-circle-fill")) %>%
+                                tooltip(
+                                    "This is where you upload the dataset on which you want to run data quality checks. In the next version of this application, there will be error checks at this point to make sure that the dataset is in the right format/encoding",
+                                    placement = "auto"
+                                ),
+                            accept = ".csv",
+                            placeholder = "mydata.csv"
+                        ),
+                        span(
+                            "Currently, this application only accepts .csv files. Please make sure your file is in the .csv format before uploading.",
+                            style = "color: #593196; font-size: 12px;"
                         )
+                    ),
+                    card(
+                        uiOutput("use_test_data_button")
+                    ),
+                    card(
+                        actionButton("show_databricks_modal", "Connect to Database", icon = icon("database"), class = "btn btn-outline-success btn-lg", width = "100%")
                     )
-                }
-            })
-            
+                )
+            } else {
+                card(
+                    actionButton(
+                        "clear_dataset",
+                        "Restart with New Data",
+                        icon = icon("repeat"),
+                        class = "btn btn-outline-primary btn-lg",
+                        width = "100%"
+                    )
+                )
+            }
+        })
+        
 DATABRICKS_SERVER_HOSTNAME <- Sys.getenv("DATABRICKS_SERVER_HOSTNAME")
 DATABRICKS_HTTP_PATH <- Sys.getenv("DATABRICKS_HTTP_PATH")
 
